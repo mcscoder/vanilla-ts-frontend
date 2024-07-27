60e08c2dcb476e068884576cc36e5381a74c7f97
mcs <maicongson69@gmail.com>
2024-07-27 14:06:08 +0700

    add: flexible proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  build: {
-    target: "esnext", //browsers can handle the latest ES features
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+  const proxyUrl =
+    mode == "development"
+      ? "http://localhost:3000"
+      : "https://json-server-jhc1.onrender.com";
+
+  return {
+    build: {
+      target: "esnext", //browsers can handle the latest ES features
     },
-  },
+    server: {
+      proxy: {
+        "/api": proxyUrl,
+      },
+    },
+  };
 });
